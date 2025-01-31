15d638e2f0b453a497a8ba2961bfe48adcabaf4f
Nam Fam <102389548+namwasinyourheart@users.noreply.github.com>
2025-02-01 01:46:32 +0700

    src: update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,4 +1,3 @@
-# %%writefile prepare_data.py
 import os
 import shutil
 import joblib
@@ -8,6 +7,8 @@ from typing import Tuple
 import logging
 
 from functools import partial
+
+from datasets import load_dataset
 
 from src.utils.utils import load_args, update_yaml
 from src.utils.model_utils import load_tokenizer
@@ -49,36 +50,6 @@ def create_dataset_dict(data_path,
         })
     return dataset_dict
 
-# def show_dataset_examples(dataset_dict):
-#     """
-#     Prints the length, columns, and an example from each split of a DatasetDict (train, val, test).
-
-#     Parameters
-#     ----------
-#     dataset_dict : datasets.DatasetDict
-#         A DatasetDict containing train, val, and test splits.
-
-#     Returns
-#     -------
-#     None
-#     """
-#     for split_name, dataset in dataset_dict.items():
-#         # Get the length and columns of the current split
-#         dataset_length = len(dataset)
-#         dataset_columns = dataset.column_names
-
-#         print(f"\nSplit: {split_name}")
-#         print(f"Number of Examples: {dataset_length}")
-#         print(f"Columns: {dataset_columns}")
-
-#         # Get the first example from the current split
-#         example = dataset[0]
-
-#         print(f"An example:")
-#         for key, value in example.items():
-#             print(f"  {key}: {value}")
-
-#     print("\n" + "=" * 40 + "\n")
 
 def show_dataset_examples(dataset_dict):
     """
@@ -149,6 +120,8 @@ def has_system_role_support(tokenizer):
 def create_prompt_formats(example,
                           tokenizer,
                           use_model_chat_template,
+                          input_col: str="question",
+                          output_col: str="answer",
                           instruction_key: str = "### Instruction:",
                           instruction_text: str = "You are a knowledgeable assistant for the company CMC Global. Your task is to providing accurate and helpful answers to the user's questions about the company.",
                           input_key: str = "### Question:",
@@ -158,9 +131,9 @@ def create_prompt_formats(example,
                           max_length = None, 
 ):
     instruction = f'{instruction_key}\n{instruction_text}'
-    input = f'{input_key}\n{example["question"]}'
+    input = f'{input_key}\n{example[input_col]}'
 
-    response = f'{response_key}\n{example["answer"]}'
+    response = f'{response_key}\n{example[output_col]}'
     
     if not end_key:
         end_key = tokenizer.eos_token
@@ -211,47 +184,18 @@ def save_dataset(dataset, save_path):
     
 def get_data_collator():
     pass
-    
-# def prepare_data(dataset_dict, template_file, keys):
-#     """Prepare the dataset by formatting the data using the provided template."""
-#     # Sample dataset (this should be loaded from your actual data source)
-#     # dataset_dict = {
-#     #     "train": [
-#     #         {"question": "What is the capital of France?", "answer": "Paris", "question_type": "geography"},
-#     #         {"question": "Who is the CEO of Tesla?", "answer": "Elon Musk", "question_type": "business"},
-#     #     ]
-#     # }
-
-#     # Load the template from the file
-#     template = load_template(template_file)
-
-#     # Prepare the output list
-#     output_texts = []
-
-#     # Format the texts based on the template
-#     for split_name in dataset_dict.keys():
-#         for example in dataset_dict[split_name]:
-            
-#             formatted_text = template.format(
-#                 **{key: example[key] for key in keys}
-#             )
-
-            
-#             output_texts.append(formatted_text)
-
-#     # Save the processed texts to a file
-#     with open('processed_data.txt', 'w') as f:
-#         f.write("\n\n".join(output_texts))
-
-#     print("Data processing complete. Output saved to 'processed_data.txt'.")
 
 def prepare_data(exp_args, data_args, model_args):
 
-    dataset_dict = create_dataset_dict(data_args.dataset.data_path, 
-                                       data_args.dataset.do_split, 
-                                       data_args.dataset.val_ratio, 
-                                       data_args.dataset.test_ratio, 
-                                       exp_args.seed)
+    if data_args.dataset.is_dataset_dict:
+        dataset_dict = load_dataset(data_args.dataset.data_path)
+    
+    else:    
+        dataset_dict = create_dataset_dict(data_args.dataset.data_path, 
+                                           data_args.dataset.do_split, 
+                                           data_args.dataset.val_ratio, 
+                                           data_args.dataset.test_ratio, 
+                                           exp_args.seed)
     
     tokenizer = load_tokenizer(data_args, model_args)
 
@@ -259,6 +203,8 @@ def prepare_data(exp_args, data_args, model_args):
         create_prompt_formats,
           tokenizer = tokenizer,
           use_model_chat_template = data_args.prompt.use_model_chat_template,
+          input_col = data_args.dataset.input_col,
+          output_col = data_args.dataset.output_col,
           instruction_key = data_args.prompt.instruction_key, # "### Instruction:",
           instruction_text = data_args.prompt.instruction_text, # "You are a knowledgeable assistant for the company CMC Global. Your task is to providing accurate and helpful answers to the user's questions about the company.",
           input_key = data_args.prompt.input_key, #"### Question:",
@@ -280,88 +226,111 @@ def prepare_data(exp_args, data_args, model_args):
 
     return dataset, save_path
 
-# if __name__ == "__main__":
-#     # Load parameters from params.yaml
-#     exp_args = load_args('configs/exp.yaml')
 
-#     data_cfg_path = exp_args.exp_manager.prepare_data_cfg_path
-#     data_args = load_args(data_cfg_path)
+def setup_environment() -> None:
+    from dotenv import load_dotenv
+    # print("SETTING UP ENVIRONMENT...")
+    _ = load_dotenv()
 
-#     train_cfg_path = exp_args.exp_manager.train_cfg_path
-#     train_args = load_args(train_cfg_path)
+def main():
 
-#     model_args = train_args.model_args
+    from hydra import initialize, compose
+    from hydra.utils import instantiate
     
+    from omegaconf import OmegaConf
+    
+    from src.utils.utils import load_args
 
-#     exps_dir = exp_args.exp_manager.exps_dir
-#     exp_name = exp_args.exp_manager.exp_name
-#     os.makedirs(exps_dir, exist_ok=True)
+    from src.utils.log_utils import init_logging, setup_logger
+    from src.utils.exp_utils import create_exp_dir
 
-#     exp_dir = os.path.join(exps_dir, exp_name)
-#     os.makedirs(exp_dir, exist_ok=True)
+    # Setup logging
+    # init_logging()
+    logger = setup_logger("ft_llm")
 
-#     exp_config_dir = os.path.join(exp_dir, 'configs')
-#     os.makedirs(exp_config_dir, exist_ok=True)
-
-#     shutil.copy('configs/exp.yaml', exp_config_dir)
-#     shutil.copy('configs/train.yaml', exp_config_dir)
-#     shutil.copy(data_cfg_path, exp_config_dir)
-
-#     # shutil.copytree('./configs', exp_config_dir)
-
-#     data_dir = os.path.join(exp_dir, 'data')
-#     os.makedirs(data_dir, exist_ok=True)
-
-#     dataset_dict = create_dataset_dict(data_args.dataset.data_path, 
-#                                        data_args.dataset.do_split, 
-#                                        data_args.dataset.val_ratio, 
-#                                        data_args.dataset.test_ratio, 
-#                                        exp_args.exp_manager.seed)
-
-#     # show_dataset_examples(dataset_dict)
-
-#     # from utils import load_tokenizer
-#     # tokenizer = load_tokenizer(data_args.tokenizer.model_name_or_path)
-#     logging.info("Loading tokenizer...")
-#     tokenizer = load_tokenizer(data_args, model_args)
+    # Setup environment
+    logger.info("SETTING UP ENVIRONMENT...")
+    setup_environment()
 
 
-#     # from functools import partial
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Process experiment configurations.')
+    parser.add_argument(
+        '--config_path',
+        type=str,
+        required=True,
+        help='Path to the configuration file for the experiment.'
+    )
 
-#     _create_prompt_formats = partial(
-#         create_prompt_formats,
-#           tokenizer = tokenizer,
-#           use_model_chat_template = data_args.prompt.use_model_chat_template,
-#           instruction_key = "### Instruction:",
-#           instruction_text = "You are a knowledgeable assistant for the company CMC Global. Your task is to providing accurate and helpful answers to the user's questions about the company.",
-#           input_key = "### Question:",
-#           response_key = "### Answer:",
-#           end_key = data_args.prompt.end_key,
-#           do_tokenize = data_args.tokenizer.do_tokenize, 
-#           max_length = data_args.tokenizer.max_length
-#     )
-#     # if params.tokenizer.do_tokenize:
-#     dataset = dataset_dict.map(
-#         _create_prompt_formats, 
-#          batched=False, 
-#          remove_columns=dataset_dict['train'].column_names
-#     )
-#     output_path = data_args.dataset.prepared_data_path
-#     if not output_path:
-#         save_path =  os.path.join(data_dir, f'{exp_name}.pkl')
-#         update_yaml(os.path.join(exp_config_dir, os.path.basename(data_cfg_path)), 'dataset.output_path', save_path)
+    # args = parser.parse_args()
+    args, override_args = parser.parse_known_args()  # Capture any extra positional arguments (overrides)
+
+    logger.info("LOADING CONFIGS...")
+
+    # Normalize the provided config path
+    config_path = os.path.normpath(args.config_path)  # Normalize path for the current OS
+
+    # Check if the configuration file exists
+    if not os.path.isfile(config_path):
+        raise FileNotFoundError(f"Configuration file not found at: {config_path}")
+
+    # Extract directory and filename from the provided config path
+    config_dir = os.path.dirname(config_path)
+    config_fn = os.path.splitext(os.path.basename(config_path))[0]
+
+    # print(config_dir)
+    # print(config_fn)
+
+    try:
+        with initialize(version_base=None, config_path=config_dir):
+            # Compose the configuration with optional overrides
+            cfg = compose(config_name=config_fn, overrides=override_args)
+    except Exception as e:
+        raise RuntimeError(f"Failed to load configuration from {config_path}: {e}")
+
+    # Print the configuration for debugging
+    # print("type(cfg):", type(cfg))
+    print(OmegaConf.to_yaml(cfg))
+
+    assert os.path.basename(config_path).replace('.yaml', '') == cfg.exp_manager.exp_name
+    
+    logger.info("CREATING EXP DIR...")
+    
+    exp_name = cfg.exp_manager.exp_name
+    # create_exp_dir(os.path.basename(config_path).replace('.yaml', ''))
+    exp_dir, configs_dir, data_dir, checkpoints_dir, results_dir  = create_exp_dir(exp_name)
+
+    import shutil
+    shutil.copy(config_path, configs_dir)
+
+    exp_args = cfg.exp_manager
+    train_args = cfg.train
+    data_args = cfg.prepare_data
+    model_args = cfg.prepare_model
+
+
+    if data_args.dataset.is_prepared:
+        # Get the path to the processed data
+        processed_data_path = os.path.normpath(data_args.dataset.output_path)
         
-#     else:
-#         save_path = output_path
-         
-#     save_dataset(dataset, save_path)
+        # Check if the processed data exists
+        if not os.path.isfile(processed_data_path):
+            raise FileNotFoundError(f"Processed data not found at: {processed_data_path}")
+        
+        # Load the dataset
+        logger.info("LOADING PROCESSED DATASET...")
+        dataset = joblib.load(processed_data_path)
+    else:
+        # Prepare dataset
+        logger.info("PREPARING DATASET...")
+        from prepare_data import prepare_data
+        dataset, processed_data_path = prepare_data(exp_args, data_args, model_args)
 
-#     show_dataset_examples(dataset)
+    # print(dataset)
+    
+    # Show dataset examples
+    show_dataset_examples(dataset)
 
-    # shutil.copy(data_cfg_path, exp_config_dir)
-    
-    # print(tokenized_dataset)
-    # print(tokenized_dataset['train'][0])
-    # show_dataset_examples(tokenized_dataset)
-    
-    # prepare_data(dataset_dict, custom_template_path, params.prompt.template_keys)
+
+if __name__ == '__main__':
+    main()
